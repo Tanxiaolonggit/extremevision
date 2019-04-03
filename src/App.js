17abaeb831d1a936b1eaca2eecba06990b8aca55
Tanxiaolonggit <997466356@qq.com>
2019-04-03 19:53:18 +0800

    txl

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 //引入路由组件
-import {Route,Link,Redirect,Switch,withRouter} from 'react-router-dom'
+import {Route,NavLink,Link,Redirect,Switch,withRouter} from 'react-router-dom'
 //引入循环渲染路由模板
 import MyRoute from './components/MyRoute'
 //引入home组件
@@ -29,20 +29,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      		<div>
-      				<Link to="/store">算法商城</Link>
-      				<Link to="/case">行业方案</Link>
-      				<Link to="/show">典型案例</Link>
-      				<Link to="/honor">资质荣誉</Link>
-      				<Link to="/coop">商务合作</Link>
-      				<Switch>
-	      				{
-	      					this.props.navs.map((item,index)=>{
-	      						return <MyRoute key={index} path={item.path} component={item.component} />
-	      		 			})
-	      				}
-	      				<Redirect from="/" to="/home" exact />
-      				</Switch>
+      		<div >
+      			<div className="headerOut">
+      				<div className="header">
+		      			<Link to="/home"><img src={require('./assets/img/logo.png')}/></Link>
+		      			<ul className="nav">
+		      				<li><NavLink to="/store">算法商城</NavLink></li>
+		      				<li><NavLink to="/case">行业方案</NavLink></li>
+		      				<li><NavLink to="/show">典型案例</NavLink></li>
+		      				<li><NavLink to="/honor">资质荣誉</NavLink></li>
+		      				<li><NavLink to="/coop">商务合作</NavLink></li>
+		      			</ul>
+	      			</div>
+      			</div>
+      			<Switch>
+	      			{
+	      				this.props.navs.map((item,index)=>{
+	      					return <MyRoute key={index} path={item.path} component={item.component} />
+	      		 		})
+	      			}
+	      			<Redirect from="/" to="/home" exact />
+      			</Switch>
       		</div>
         	
       </div>
