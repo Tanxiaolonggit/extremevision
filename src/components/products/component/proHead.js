0b2792e51cd7c6132df289d2498a7b5fc0db04cf
Tanxiaolonggit <997466356@qq.com>
2019-04-08 19:28:05 +0800

    ww2

--- src/components/products/component/proHead.js
+++ src/components/products/component/proHead.js
@@ -6,18 +6,42 @@ class ProHead extends Component{
 	render(){
         let {hlist,type}=this.props;
 		return (
-			<section>
-                <header className="proH">
-                   <h1>{hlist[type].title}</h1>
-                  <ul> 
-                   {
-                      hlist[type].conlist.map((v,i)=>{
-                          return <li key={i}>{v}</li>
-                      })
-                    }
-                  </ul>
-                </header>
-			</section>
+			<div>
+				<section>
+	                <header className="proH">
+		                <div>
+		                   <h1>{hlist[type].title}</h1>
+			                  <ul> 
+			                    {
+			                       hlist[type].conlist.map((v,i)=>{
+			                           return <li key={i}>{v}</li>
+			                       })
+			                     }
+			                   </ul>
+		                </div>
+	                </header>     
+				</section>
+				    <div className="neck">
+						<div>
+							<span>
+								<img src="https://static2.extremevision.com.cn/images/lion/products/security/icon_1.png" alt="无人智慧安防降低隐患" />
+								<p>无人智慧安防降低隐患</p>
+							</span>
+							<span>
+								<img src="https://static2.extremevision.com.cn/images/lion/products/pass_flow/icon_2.png" alt="海量视频云端存储调用" />
+								<p>海量视频云端存储调用</p>
+							</span>
+							<span>
+								<img src="https://static2.extremevision.com.cn/images/lion/products/pass_flow/icon_3.png" alt="小样本数据高精度开发" />
+								<p>小样本数据高精度开发</p>
+							</span>
+							<span>
+								<img src="https://static2.extremevision.com.cn/images/lion/products/security/icon_4.png" alt="安保力量调度提升效率" />
+								<p>安保力量调度提升效率</p>
+							</span>
+						</div>
+	                </div>
+			</div>
 		)
 	}
 }
